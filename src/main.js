b96ca985e6995e9bac044d6401f7a34a4e8206f2
Belen Molina <belen.molina.z@gmail.com>
2023-02-08 17:31:15 -0500

    Mejora en codigo y comentarios

--- src/main.js
+++ src/main.js
@@ -1,24 +1,23 @@
 import { example } from './data.js';
 
-
-
-// import data from './data/lol/lol.js';
-//import data from './data/pokemon/pokemon.js';
-//importo la data de rik y morty para trabajar
 import data from './data/rickandmorty/rickandmorty.js';
+const mainGallery = characterGallery ();
+function characterGallery () {
 //declaro e inicializo elementos para ingresar nombre y foto personaje
-let elementos="";
-// con la data examino solo results que contiene a los personajes
-//se ingresa en elementos cada elemento de results que sea img y nombre que se almacena en example
-const ejemplo = data.results.forEach(elemento => {
-  elementos = elementos + `
+let elements="";
+// examino solo results que contiene a los personajes
+//templates en cada elemento de results que sea img y nombre
+data.results.forEach(element => {
+  elements = elements + `
   <div id="image">
-  <img id="photo" src=${elemento.image}>
-  <h1 id="tittle">${elemento.name}</h1>
+  <img id="photo" src=${element.image}>
+  <h1 id="tittle">${element.name}</h1>
   </div>
   `;
 });
+return elements;
+}
 //se obtiene el elemento id y con inner se presenta en document el resultado
-document.getElementById("root").innerHTML= elementos;
+document.getElementById("root").innerHTML= mainGallery;
 console.log(example, data);
 

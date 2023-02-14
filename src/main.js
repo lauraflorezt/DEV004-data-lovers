77526fcd4a3e23159b0fc2ec4a4f20cdf36768b8
Belen Molina <belen.molina.z@gmail.com>
2023-02-14 18:26:37 -0500

    Cambios filtros y estilos para presentacion

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { filterData, typeSpecies } from './data.js';
+import { filterData, typeSpecies, filterEpisode } from './data.js';
 import data from './data/rickandmorty/rickandmorty.js';
 // data de interes
 const dataRM = data.results;
@@ -10,13 +10,17 @@ const mainGallery = document.getElementById("root");
 //Presentacion total personajes
 
 const cards = function characterGallery(data) {
-  
+
   let elements = "";
   data.forEach(element => {
     elements = elements + `
   <div id="image">
+  <div id="card">
   <img id="photo" src=${element.image}>
   <h1 id="tittle">${element.name}</h1>
+  <p>Status: ${element.status}</p>
+  <p>Gender: ${element.gender}</p>
+  </div>
   </div>
   `;
   });
@@ -26,7 +30,7 @@ const cards = function characterGallery(data) {
 
 //filtro de personajes por especie
 species.addEventListener("change", function () {
-  
+
   const specie = species.value;
   const resultSpecie = filterData(dataRM, specie);
 
@@ -37,7 +41,16 @@ species.addEventListener("change", function () {
 //console.log(cards(resultSpecie))
 console.log(typeSpecies(dataRM));
 
-
 mainGallery.innerHTML = cards(dataRM);
 //console.log(example, data);
 
+
+//filtro de personajes por especie
+episodes.addEventListener("change", function () {
+
+  const episode = episodes.value;
+  const resultEpisode = filterEpisode(dataRM, episode);
+
+  console.log("result", resultEpisode);
+  mainGallery.innerHTML = cards(resultEpisode);
+})

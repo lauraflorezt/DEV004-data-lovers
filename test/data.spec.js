abe352b3f2d171adef87c11cba66093423f0fb67
Belen Molina <belen.molina.z@gmail.com>
2023-02-23 17:50:54 -0500

    test en status y episode

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { filterData,sortDataAZ,filterStatus,filterEpisode } from '../src/data.js';
+import { filterData, sortDataAZ, filterStatus, filterEpisode } from '../src/data.js';
 
 describe('sortDataAZ', () => {
   it('is a function', () => {
@@ -7,101 +7,83 @@ describe('sortDataAZ', () => {
 
   it('returns `1` si esta bien el orden', () => {
     expect(sortDataAZ([
-      {"name":"a"},{"name":"b"}])).toStrictEqual([{"name":"a"},{"name":"b"}]);//toStrictEqual Prueba misma estructura y tipo de los objetos
+      { "name": "a" }, { "name": "b" }])).toStrictEqual([{ "name": "a" }, { "name": "b" }]);//toStrictEqual Prueba misma estructura y tipo de los objetos
   });
 
   it('returns `-1` si debe intercambiar el orden', () => {
     expect(sortDataAZ([
-      {"name":"c"},{"name":"a"}])).toEqual([{"name":"a"},{"name":"c"}]);//toEqual compara propiedades de objetos
+      { "name": "c" }, { "name": "a" }])).toEqual([{ "name": "a" }, { "name": "c" }]);//toEqual compara propiedades de objetos
   });
 
   it('returns `0` si es igual los nombres ', () => {
     expect(sortDataAZ([
-      {"name":"a"},{"name":"a"}])).toStrictEqual([{"name":"a"},{"name":"a"}]); 
+      { "name": "a" }, { "name": "a" }])).toStrictEqual([{ "name": "a" }, { "name": "a" }]);
   });
 })
 
-const infData = [
-  {
-    "name": "Rick Sanchez",
-    "status": "Alive",
-    "species": "Human",
-    "type": "",
-    "gender": "Male",
-    "episode": [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2",
-      "https://rickandmortyapi.com/api/episode/3",
-      "https://rickandmortyapi.com/api/episode/4",
-      "https://rickandmortyapi.com/api/episode/5",
-      "https://rickandmortyapi.com/api/episode/6",
-      "https://rickandmortyapi.com/api/episode/7",
-    ]}
-  ,{
-    "name": "Abadango Cluster Princess",
-    "status": "Alive",
-    "species": "Alien",
-    "type": "",
-    "gender": "Female",
-    "episode": [
-      "https://rickandmortyapi.com/api/episode/1",
-      "https://rickandmortyapi.com/api/episode/2",
-      "https://rickandmortyapi.com/api/episode/3",
-      "https://rickandmortyapi.com/api/episode/4",
-      "https://rickandmortyapi.com/api/episode/5",
-      "https://rickandmortyapi.com/api/episode/6",
-      "https://rickandmortyapi.com/api/episode/7",
-    ]} 
-]
-
 describe('filterData', () => {
+
   it('is a function', () => {
     expect(typeof filterData).toBe('function');
   });
 
 
   it('returns `filterSpecies`', () => {
+    
     expect(filterData(infData, 'Human')).toEqual(
       {
         "species": "Human",
       }
-      ,{
+      , {
         "species": "Alien",
-      } 
+      }
     )
   });
 });
 
 describe('filterEpisode', () => {
+  const dataPrueba = [
+
+    { "episode": "episode/1" },
+
+    { "episode": "episode/1" },
+
+    { "episode": "episode/2" },
+
+    { "episode": "episode/3" }
+
+  ];
+  const episodePrueba = filterEpisode(dataPrueba, "episode/1")
   it('is a function', () => {
-    expect(typeof filterEpisode).toBe('function'); 
+    expect(typeof filterEpisode).toBe('function');
   });
-  it('returns `kindEpisode`', () => {
-    expect(filterEpisode(infData)).toEqual(
-      {
-        "name": "Rick Sanchez",
-      }
-      ,{
-        "name": "Abadango Cluster Princess",
-      } 
+  it('returna solo episodio 1', () => {
+    expect(episodePrueba).toEqual([{"episode": "episode/1"},{"episode": "episode/1"}] 
     )
   });
 });
 
 describe('filterStatus', () => {
+  const dataPrueba = [
+
+    { "status": "Alive" },
+
+    { "status": "Dead" },
+
+    { "status": "Unknown" }
+
+  ]
   it('is a function', () => {
     expect(typeof filterStatus).toBe('function');
   });
-  it('returns `kindStatus`', () => {
-    expect(filterStatus(infData)).toEqual(
-      {
-        "status": "Alive",
-      }
-      
-    )
+  it('retorna el array que contiene el objeto Alive', () => {
+    expect(filterStatus(dataPrueba, "Alive")).toEqual([{ "status": "Alive" }])
+  });
+  it('retorna el array vacio cuando no existe equivalencia', () => {
+    expect(filterStatus(dataPrueba, "Vivo")).toEqual([{ "status": "Alive" }])
   });
 });
 
 
-  
+
 
